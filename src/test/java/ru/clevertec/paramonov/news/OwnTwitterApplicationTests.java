c9a30c2abf63eefb927735b94ca8e32a2c3ea189
Alexey Paramonov <paramonov_tama@mail.ru>
2023-04-28 17:09:14 +0300

    init commit

---
+++ src/test/java/ru/clevertec/paramonov/news/OwnTwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clevertec.paramonov.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnTwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

1e0675961b90db870adfb5f5c29b0d6de29dedd6
Alexey Paramonov <paramonov_tama@mail.ru>
2023-04-29 00:01:50 +0300

    feat: app.prop -> app.yml. implementation postgres (build.gradle). entity-relationship-diagram.jpg (1 version). comment entity

--- src/main/java/ru/clevertec/paramonov/news/entity/Comment.java
+++ src/main/java/ru/clevertec/paramonov/news/entity/Comment.java
@@ -1,35 +1,25 @@
 package ru.clevertec.paramonov.news.entity;
 
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
-@Table(name = "news")
-public class News implements BaseEntity<Long>{
+@Table(name = "comment")
+public class Comment implements BaseEntity<Long> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "news_id")
+    @Column(name = "comment_id")
     private Long id;
-
-    private LocalDateTime time;
-
-    private String title;
-
-    @OneToMany
-    private List<Comment> comments;
 }

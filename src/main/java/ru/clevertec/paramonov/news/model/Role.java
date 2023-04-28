d9c67d563398e612479ac2fda2c04b1d866d7970
Alexey Paramonov <paramonov_tama@mail.ru>
2023-04-29 00:25:48 +0300

    feat: some javadoc for enum role.

--- src/main/java/ru/clevertec/paramonov/news/model/Role.java
+++ src/main/java/ru/clevertec/paramonov/news/model/Role.java
@@ -2,11 +2,24 @@ package ru.clevertec.paramonov.news.model;
 
 public enum Role {
 
+    /**
+     * Admin user role (can perform CRUD operations on all entities).
+     */
     ADMIN,
 
+    /**
+     * Journalist user role (can add and change/delete only his own news).
+     */
     JOURNALIST,
 
+    /**
+     * Subscriber user role (can add and change/delete only his own comments).
+     */
     SUBSCRIBER,
 
+
+    /**
+     * Unregistered user role (can only view news and comments).
+     */
     UNREGISTERED
 }

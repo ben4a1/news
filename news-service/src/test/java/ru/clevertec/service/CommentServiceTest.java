b05f0dd8e886f48ce60a40fe68116520bab34479
Alexey Paramonov <paramonov_tama@mail.ru>
2023-05-01 16:50:48 +0300

    feat: @builder for user; first test in comment service test

--- news-service/src/test/java/ru/clevertec/service/CommentServiceTest.java
+++ news-service/src/test/java/ru/clevertec/service/CommentServiceTest.java
@@ -5,14 +5,28 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import ru.clevertec.dto.CommentReadDto;
+import ru.clevertec.entity.Comment;
+import ru.clevertec.entity.User;
 import ru.clevertec.mapper.impl.CommentCreateMapper;
 import ru.clevertec.mapper.impl.CommentReadMapper;
 import ru.clevertec.repository.CommentRepository;
 import ru.clevertec.service.impl.CommentService;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static java.time.LocalDateTime.now;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+
 @ExtendWith(MockitoExtension.class)
 class CommentServiceTest {
 
+    public static final Long COMMENT_ID = 1L;
+    public static final LocalDateTime CREATION_TIME = now();
+    public static final String SUBJECT = "Pugacheva reacted to the death of rabbit";
+    private static final String USERNAME = "Max";
     @Mock
     private CommentRepository commentRepository;
     @Mock
@@ -23,7 +37,24 @@ class CommentServiceTest {
     private CommentService commentService;
 
     @Test
-    void checkFindByIdShouldReturn() {
+    void checkFindByIdShouldReturnEquals() {
+        Comment comment = Comment.builder()
+                .id(COMMENT_ID)
+                .creationTime(CREATION_TIME)
+                .subject(SUBJECT)
+                .user(User.builder().username(USERNAME).build()).build();
+
+        doReturn(Optional.of(comment))
+                .when(commentRepository).findById(COMMENT_ID);
+
+        doReturn(new CommentReadDto(COMMENT_ID, CREATION_TIME, SUBJECT, USERNAME))
+                .when(commentReadMapper).map(comment);
+
+        Optional<CommentReadDto> actualResult = commentService.findById(COMMENT_ID);
+        assertThat(actualResult).isPresent();
+
+        CommentReadDto expectedResult = new CommentReadDto(COMMENT_ID, CREATION_TIME, SUBJECT, USERNAME);
+        actualResult.ifPresent(actual -> assertThat(actual).isEqualTo(expectedResult));
     }
 
     @Test

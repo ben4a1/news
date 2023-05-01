218302c3b5ab9ef08123892e5b4539c3055eb432
Alexey Paramonov <paramonov_tama@mail.ru>
2023-05-01 15:42:44 +0300

    fix: @componet for news createmapper

--- news-service/src/main/java/ru/clevertec/mapper/impl/NewsCreateMapper.java
+++ news-service/src/main/java/ru/clevertec/mapper/impl/NewsCreateMapper.java
@@ -1,5 +1,6 @@
 package ru.clevertec.mapper.impl;
 
+import org.springframework.stereotype.Component;
 import ru.clevertec.dto.NewsCreateUpdateDto;
 import ru.clevertec.entity.News;
 import ru.clevertec.mapper.Mapper;
@@ -7,6 +8,7 @@ import ru.clevertec.mapper.Mapper;
 import static java.time.LocalDateTime.*;
 import static ru.clevertec.entity.News.*;
 
+@Component
 public class NewsCreateMapper implements Mapper<NewsCreateUpdateDto, News> {
 
     @Override

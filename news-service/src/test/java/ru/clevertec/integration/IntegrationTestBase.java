cb67a886a25f77b9806c38fb74b34affd4fa309e
Alexey Paramonov <paramonov_tama@mail.ru>
2023-05-04 17:28:19 +0300

    feat: @Sql { } for IntegrationTestBase

--- news-service/src/test/java/ru/clevertec/integration/IntegrationTestBase.java
+++ news-service/src/test/java/ru/clevertec/integration/IntegrationTestBase.java
@@ -3,10 +3,14 @@ package ru.clevertec.integration;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.containers.PostgreSQLContainer;
 import ru.clevertec.integration.annotation.IT;
 
 @IT
+@Sql({
+        "classpath:sql/data.sql"
+})
 public abstract class IntegrationTestBase {
 
     private static final PostgreSQLContainer<?> CONTAINER = new PostgreSQLContainer<>("postgres:14.1");
